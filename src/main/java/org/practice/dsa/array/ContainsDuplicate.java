6cc3339c9230b7bfc1b9cb77871f0bb3e860249b
rmishra <raghvendra.k.mishra@outlook.com>
2024-01-22 15:39:17 +0530

    Contains duplicate problem

---
+++ src/main/java/org/practice/dsa/array/ContainsDuplicate.java
@@ -0,0 +1,85 @@
+package org.practice.dsa.array;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+ *
+ * Example 1:
+ *
+ * Input: nums= [1, 2, 3, 4]
+ * Output: false
+ * Explanation: There are no duplicates in the given array.
+ * Example 2:
+ *
+ * Input: nums= [1, 2, 3, 1]
+ * Output: true
+ * Explanation: '1' is repeating.
+ */
+public class ContainsDuplicate {
+
+    /**
+     * TC: O(N^2), SC: O(1)
+     */
+    boolean usingBruteForce(int[] numbers) {
+        // return false when input is null or length is <= 1
+        if(numbers == null || numbers.length <= 1) {
+            return false;
+        }
+
+        // compare each element to all other elements
+        for(int i = 0; i < numbers.length - 1; i++) {
+            for(int j = i + 1; j < numbers.length; j++) {
+                if(numbers[i] == numbers[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * TC: O(N), SC: O(N)
+     */
+    boolean usingHashSet(int[] numbers) {
+        // return false when input is null or length is <= 1
+        if(numbers == null || numbers.length <= 1) {
+            return false;
+        }
+        Set<Integer> uniqueSet = new HashSet<>();
+        for(int number : numbers) {
+            if(!uniqueSet.add(number)) { // when set.add() returns false, means unable to add element since it already exist
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * TC: O(NlogN), SC: O(1) or O(N)  -> depending on sorting algorithm used
+     */
+    boolean usingSort(int[] numbers) {
+        // return false when input is null or length is <= 1
+        if(numbers == null || numbers.length <= 1) {
+            return false;
+        }
+        // iterate to validate the already sorted case
+        for(int i = 1; i < numbers.length; i++) {
+            if(numbers[i-1] == numbers[i]) {
+                return true;
+            }
+        }
+
+        // sort the array
+        Arrays.sort(numbers);
+        for(int i = 1; i < numbers.length; i++) {
+            if(numbers[i-1] == numbers[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ src/test/java/org/practice/dsa/array/ContainsDuplicateTest.java
@@ -0,0 +1,52 @@
+package org.practice.dsa.array;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+class ContainsDuplicateTest {
+
+    private final int[] sample1 = null;
+    private final int[] sample2 = {};
+    private final int[] sample3 = {1,2,3,4};
+    private final int[] sample4 = {1,2,3,1};
+    private final int[] sample5 = {1,1,1,1};
+
+    private final ContainsDuplicate object = new ContainsDuplicate();
+
+    @Test
+    void usingBruteForce() {
+        assertFalse(object.usingBruteForce(sample1));
+        assertFalse(object.usingBruteForce(sample2));
+        assertFalse(object.usingBruteForce(sample3));
+
+        assertTrue(object.usingBruteForce(sample4));
+        assertTrue(object.usingBruteForce(sample5));
+    }
+
+    @Test
+    void usingHashSet() {
+        assertFalse(object.usingHashSet(sample1));
+        assertFalse(object.usingHashSet(sample2));
+        assertFalse(object.usingHashSet(sample3));
+
+        assertTrue(object.usingHashSet(sample4));
+        assertTrue(object.usingHashSet(sample5));
+    }
+
+    @Test
+    void usingSort() {
+        assertFalse(object.usingSort(sample1));
+        assertFalse(object.usingSort(sample2));
+        assertFalse(object.usingSort(sample3));
+
+        assertTrue(object.usingSort(sample4));
+        assertTrue(object.usingSort(sample5));
+    }
+
+    
+}
